68d421c5151b23f1bf44f009d43979a17c8e0a73
阿部直也 <n.abe@gemcook.com>
2020-01-25 10:56:02 +0900

    aaas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I love micropig!!!!</p>
+        <p>I </p>
         <a
           className="App-link"
           href="https://reactjs.org"
